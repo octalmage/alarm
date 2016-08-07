73838e09a3880a7cbb8808fe63b576b7f7403556
Taryn Greer <therealtaryn@gmail.com>
2016-08-07 16:37:19 -0500

    Butt Hand

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 var storage = require('node-persist');
-var mustacheExpress = require('mustache-express');
+//var mustacheExpress = require('mustache-express');
+var hbs = require('hbs');
+app.use(express.static('assets/public'));
 
 // Register '.mustache' extension with The Mustache Express
-app.engine('html', mustacheExpress());
+app.engine('html', hbs.__express);
+// check helper
+hbs.registerHelper("checked", function( array, value, options ){
+	return (array.indexOf(value) > -1) ? 'checked="checked"' : '';
+});
 
 app.set('view engine', 'html');
 app.set('views', __dirname + "/assets/views/");
@@ -14,18 +20,16 @@ app.use(express.static('public'));
 
 app.get('/', function (req, res) {
 	var time = storage.getItem('time');
-	console.log(time);
-res.render('index', { time: time});
+	var days = storage.getItem('days');
+res.render('index', { time: time, days: days });
 });
 
 
 app.get('/save', function (req, res) {
 
 	// Prepare output in JSON format
-	response = {
-		time:req.query.usr_time,
-	};
 	storage.setItem('time',req.query.usr_time);
+	storage.setItem('days',req.query.days);
 	res.redirect('/');
 });
 
